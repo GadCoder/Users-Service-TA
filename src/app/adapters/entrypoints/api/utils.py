a1c7ec8a4003e229e1fa03c4e93fa8679bdd9482
german <german.ampuero@unmsm.edu.pe>
2024-12-01 12:21:23 -0500

    please lord give me strength to continue. pd: this might fix auth when doing http requests

--- src/app/adapters/entrypoints/api/utils.py
+++ src/app/adapters/entrypoints/api/utils.py
@@ -26,12 +26,17 @@ class OAuth2PasswordBearerWithCookie(OAuth2):
 
         scheme, param = get_authorization_scheme_param(authorization)
         if not authorization or scheme.lower() != "bearer":
-            if self.auto_error:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Not authenticated",
-                    headers={"WWW-Authenticate": "Bearer"},
-                )
+            authorization: Optional[str] = request.headers.get("Authorization")
+            scheme, param = get_authorization_scheme_param(authorization)
+            if not authorization or scheme.lower() != "bearer":
+                if self.auto_error:
+                    raise HTTPException(
+                        status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="Not authenticated",
+                        headers={"WWW-Authenticate": "Bearer"},
+                    )
+                else:
+                    return None
             else:
-                return None
+                return param
         return param
