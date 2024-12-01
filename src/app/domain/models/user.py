b4369823c485772ad23d1d3540d2212f682a0932
german <german.ampuero@unmsm.edu.pe>
2024-12-01 02:51:17 -0500

    increase code size to 20

--- src/app/domain/models/user.py
+++ src/app/domain/models/user.py
@@ -7,7 +7,7 @@ from src.app.domain.schemas.user_base import UserBase
 
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    code: str = Field(max_length=6, nullable=False, unique=True)
+    code: str = Field(max_length=20, nullable=False, unique=True)
     hashed_password: str = Field(nullable=False)
     is_teacher: bool = Field(default=True)
     is_active: bool = Field(default=True)
