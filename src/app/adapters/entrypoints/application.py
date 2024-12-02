d84184335806a6e60683bac42dec8fb5be1631e2
german <german.ampuero@unmsm.edu.pe>
2024-12-02 18:31:00 -0500

    add cors

--- src/app/adapters/entrypoints/application.py
+++ src/app/adapters/entrypoints/application.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from src.app.adapters.entrypoints.api.base import api_router
 from src.app.adapters.db.orm import create_db_and_tables
 from src.app.configurator.config import settings
@@ -10,12 +10,24 @@ def include_router(app_):
     app_.include_router(api_router)
 
 
+def add_cors(app_):
+    origins = ["*"]
+    app_.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
 def start_application():
     container = Container()
     app_ = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     app_.container = container
     include_router(app_)
     create_db_and_tables()
+    add_cors(app_)
     return app_
 
 
